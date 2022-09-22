648461b6f77fdd57d6c607ee7038729ebe52c692
Yiyang Chen <yiyangc@berkeley.edu>
2022-09-22 15:08:37 -0700

    Added crsp fetch code

--- src/test_wrds.py
+++ src/test_wrds.py
@@ -1,14 +1,38 @@
 import wrds
+import time
 
 if __name__ == '__main__':
 
+	command_log = ""
 	db = wrds.Connection(wrds_username='yiyangc')
 
-	command = "print('WRDS Interactive Session...')"
+	print()
+	command = "print('\u001b[7m WRDS Interactive Session \u001b[0m')"
 	while command != "q":
+
+		if command == "s":
+			f = open(f"logs/log-{time.strftime('%Y%m%d-%H%M%S', time.localtime())}.txt", "w")
+			f.write(command_log)
+			f.close()
+			print('\u001b[93m'+"Commands saved. "+'\u001b[0m')
+
+			command = input(">>> ")
+			continue;
+
 		try:
+			if "db.get_table" in command or "db.raw_sql" in command:
+				print('\u001b[93m'+"Fetching data..."+'\u001b[0m')
 			exec(command)
-		except:
-			print('\033[91m'+"An error occurred during execution. "+'\033[0m')
+
+			command_log += "[Executed] " + command + "\n"
+
+		except Exception as e:
+			print('\u001b[91m'+"An error occurred during execution. "+'\u001b[0m')
+			print('\u001b[91m'+str(e)+'\u001b[0m')
+
+			command_log += "[Failed] " + command + "\n"
+
 		command = input(">>> ")
+
+	db.close()
 
