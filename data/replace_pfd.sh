f7a9a2dc3815f034b60d74263d8d61f264658c6e
Yiyang Chen <yiyangc@berkeley.edu>
2022-10-07 12:18:26 -0700

    Added first stage pfd data cleaning and coverage report

--- data/replace_pfd.sh
+++ data/replace_pfd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo duplicating...
+cp -p PFDtrans.txt PFDtrans_test.txt
 echo replacing...
-sed -i s/\|//g pfd/PFDtrans_test.txt
+sed -i s/\,\,/\,\|\|\,/g PFDtrans_test.txt
+sed -i s/\|\,\|/\\t/g PFDtrans_test.txt
+sed -i s/\|//g PFDtrans_test.txt
 echo done.
 
