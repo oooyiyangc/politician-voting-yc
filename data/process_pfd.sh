6bc4d751db1ff8e5dd0c3c51ebbd2792f9a2e368
Yiyang Chen <yiyangc@berkeley.edu>
2022-10-27 23:15:45 -0700

    Fixed process_pfd.sh bug and exported cleaned PFDtrans as csv

--- data/process_pfd.sh
+++ data/process_pfd.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# echo processing PFDtrans
-# echo duplicating...
-# cp -p pfd/PFDtrans.txt pfd/PFDtrans_test2.txt
-# echo replacing...
-# sed -i s/\,\,/\,\|\|\,/g pfd/PFDtrans_test2.txt  # replacing ,, with ,||, 
-# sed -i s/\|\,\|/\\t/g pfd/PFDtrans_test2.txt     # replacing |,| with tab
-# sed -i s/\|\,/\\t/g pfd/PFDtrans_test2.txt       # replacing |, with tab
-# sed -i s/\,\|/\\t/g pfd/PFDtrans_test2.txt       # replacing ,| with tab
-# sed -i s/\|//g pfd/PFDtrans_test2.txt	         # removing | at the beginning and at the end
-# echo done.
+echo Processing PFDtrans
+echo duplicating...
+cp -p pfd/PFDtrans.txt pfd/PFDtrans_test2.txt
+echo replacing...
+sed -i s/\|\,\,\|/\|\,\|\|\,\|/g pfd/PFDtrans_test2.txt  # replacing |,,| with |,||,| 
+sed -i s/\|\,\|/\\t/g pfd/PFDtrans_test2.txt             # replacing |,| with tab
+sed -i s/\|\,/\\t/g pfd/PFDtrans_test2.txt               # replacing |, with tab
+sed -i s/\,\|/\\t/g pfd/PFDtrans_test2.txt               # replacing ,| with tab
+sed -i s/\|//g pfd/PFDtrans_test2.txt	                 # removing | at the beginning and at the end
+echo done.
 
-echo processing PFDasset
+echo Processing PFDasset
 echo duplicating...
 cp -p pfd/PFDasset.txt pfd/PFDasset_test2.txt
 echo replacing...
-sed -i s/\,\,/\,\|\|\,/g pfd/PFDasset_test2.txt  # replacing ,, with ,||, 
-sed -i s/\|\,\|/\\t/g pfd/PFDasset_test2.txt     # replacing |,| with tab
-sed -i s/\|\,/\\t/g pfd/PFDasset_test2.txt       # replacing |, with tab
-sed -i s/\,\|/\\t/g pfd/PFDasset_test2.txt       # replacing ,| with tab
-sed -i s/\|//g pfd/PFDasset_test2.txt	         # removing | at the beginning and at the end
+sed -i s/\|\,\,\|/\|\,\|\|\,\|/g pfd/PFDasset_test2.txt  # replacing |,,| with |,||,| 
+sed -i s/\|\,\|/\\t/g pfd/PFDasset_test2.txt             # replacing |,| with tab
+sed -i s/\|\,/\\t/g pfd/PFDasset_test2.txt               # replacing |, with tab
+sed -i s/\,\|/\\t/g pfd/PFDasset_test2.txt               # replacing ,| with tab
+sed -i s/\|//g pfd/PFDasset_test2.txt	                 # removing | at the beginning and at the end
 echo done.
 
 echo finished. 
